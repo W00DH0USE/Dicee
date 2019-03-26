190b86060f4533d9be7a14dc4bdb71469d5a1ad4
Iyron <iyronwoodhouse@gmail.com>
2019-03-26 11:31:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// DICE ONE
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll('img')[0];
+
+image1.setAttribute("src", randomDiceImage);
+
+
+// DICE TWO
+
+var randomNumber2 = Math.floor(Math.random() * 6 ) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll('img')[1];
+
+image2.setAttribute("src", randomDiceImage2);
+
+// TITLE CHANGE
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = "🚩 Player 1 Wins!"
+}
+if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩"
+}
+if (randomNumber1 === randomNumber2) {
+  document.querySelector('h1').innerHTML = "🏳️ It's a Draw! 🏳️"
+}
